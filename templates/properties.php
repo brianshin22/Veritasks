0822e3222e32d3dca6cbef71af8ad34247007f57
Brian Shin <brianshin22@hotmail.com>
2013-01-23 19:38:35 -0500

    huh

--- templates/properties.php
+++ templates/properties.php
@@ -1,60 +1,41 @@
 <!-- Main page that user accesses. Contains information about all tasks available-->
 
-<script src="js/makeIntoTable.js"> </script>
+<script type="text/javascript" src="js/rowClick.js">    
+</script>
+
+
+<div>
+    <h3>List of Properties</h3>
+</div>
+
 
 <div class="row">
 <div class="span10 offset2">
-<h3>Available Properties</h3>
+    
 
 <table id="tasktable" class="table table-hover ">
     <thead>
-        <tr>
-            <th>Title</th>      
-            <th>Description</th>
-            <th>Address</th>
-            <th>Contact owner</th>    
-         </tr>
     </thead>
     <tbody>
+
+    
     <?php foreach ($rows as $row): ?>
         <tr>  
-            <td><?= $row["title"] ?></td>  
-            <td><?= $row["Description"] ?></td>   
-            <td><?= $row["Address"] ?></td> 
             <td>
-                <form action="index.php" method="post">
-                    <input type="hidden" name="hid1" value="<?= $row['propertyid'] ?>">
-                    <button class="btn" type="submit" value="Go to Task">Go to Task</button>
-                </form>
-            </td>   
+                <p><?=$row["title"]?></p>
+                <a href="/page.php?id=<?=$row['propertyid']?>" >
+                </a>
+            </td>
+            
+            
         </tr>
-    <? endforeach ?>
+    <? endforeach ?> 
     </tbody>
 </table>
 
-<script src="galleria/galleria-1.2.9.min.js"></script>
-<style>
-    #galleria{ width: 800; height: 400px; background: #000 }
-</style>
-
-<div id="galleria">
-<?php 
-
-$dirname = "uploads/"; 
-$images = scandir($dirname); 
-$ignore = Array(".", "..", "otherfiletoignore");
-foreach($images as $curimg){
-if(!in_array($curimg, $ignore)) {
-echo "<img src='uploads/$curimg' />\n";
-}; 
-}  
-     
-?> 
-</div>
-<script>
-    Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
-    Galleria.run('#galleria');
-</script>
-
 </div>
 </div>
+
+
+
+
